fadf19901c78f790419934d1d8481107cb083364
Phayuth <70325209+Phayuth@users.noreply.github.com>
2021-10-26 10:12:50 +0700

    Update Manual_Stick.py

--- Manual Control/Manual_Stick.py
+++ Manual Control/Manual_Stick.py
@@ -15,34 +15,32 @@ PWM.start(Thrtpin, 0, 50)
 PWM.start(Strgpin, 0, 50)
 
 try:
-    dc = 0
-    StrgV = 0
-    
-    while not joy.Back():
-        ThrtV = joy.leftY()
-        StrgV = joy.rightX()
-        
-        if ThrtV < 0:
-            dc = ((ThrtV+1)*(6.1-5.4))+5.4 # map funtion
-        if ThrtV > 0:
-            dc = ((ThrtV-0)*(9-6.1))+6.1 # map funtion
-        
-        #ESC PWM arm = 6.1, Max reverse = 5.4, Max forward = 9
-        if dc <5.4:
-            dc = 5.4
-        if dc > 9:
-            dc = 9
-            
-        PWM.set_duty_cycle(Thrtpin, dc)
-        PWM.set_duty_cycle(Strgpin, abs(StrgV)*10)
-        time.sleep(0.01)
-    
-    PWM.stop(Thrtpin)
-    PWM.stop(Strgpin)
-    PWM.cleanup()
-    
+	thr = 0
+	stg = 0
+	
+	while not joy.Back():
+		ThrtV = joy.leftY()
+		StrgV = joy.rightX()
+		
+		# Throttle mapping : ESC PWM arm = 6.1, Max reverse = 5.4, Max forward = 9
+		if ThrtV < 0:
+			thr = ((ThrtV+1)*(6.1-5.4))+5.4 # map funtion
+		if ThrtV > 0:
+			thr = ((ThrtV-0)*(9-6.1))+6.1 # map funtion
+		
+		# Steering mapping : Servo
+		stg = ((StrgV+1)*(out_max-out_min)/2)+out_min
+			
+		PWM.set_duty_cycle(Thrtpin, thr)
+		PWM.set_duty_cycle(Strgpin, stg)
+		time.sleep(0.01)
+	
+	PWM.stop(Thrtpin)
+	PWM.stop(Strgpin)
+	PWM.cleanup()
+	
 except KeyboardInterrupt:
-    print("End")
-    PWM.stop(Thrtpin)
-    PWM.stop(Strgpin)
-    PWM.cleanup()
+	print("End")
+	PWM.stop(Thrtpin)
+	PWM.stop(Strgpin)
+	PWM.cleanup()
